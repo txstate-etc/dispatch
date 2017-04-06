1b07b71c5b6f765ce5757d9a35824338d321aba6
wickning1 <git@mythgaming.net>
2017-04-06 10:16:09 -0500

    cleaned up our jsontime object, output in rfc3339 like javascript would do

--- jsontime.go
+++ jsontime.go
@@ -11,24 +11,22 @@ type JsonTime struct {
 func (self *JsonTime) UnmarshalJSON(b []byte) (err error) {
 	s := string(b)
 	s = s[1:len(s)-1]
-	t, err := time.Parse(time.RFC3339Nano, s)
-	if err != nil {
-		t, err = time.Parse("2006-01-02 15:04:05-0700", s)
-	}
-	if err != nil {
-		t, err = time.Parse("2006-01-02 15:04:05-07:00", s)
-	}
-	if err != nil {
-		t, err = time.Parse("2006-01-02T15:04:05-0700", s)
-	}
-	if err != nil {
-		t, err = time.Parse("20060102150405MST", s)
+	formats := [...]string{
+		time.RFC3339Nano, time.RFC3339, time.RFC1123,
+		"2006-01-02 15:04:05-0700",
+		"2006-01-02 15:04:05-07:00",
+		"2006-01-02T15:04:05-0700",
+		"20060102150405MST" }
+	var t time.Time
+	for _,format := range formats {
+		t, err = time.Parse(format, s)
+		if err == nil { break }
 	}
 	self.Time = t
 	return err
 }
 
 func (self *JsonTime) MarshalJSON() ([]byte, error) {
-	s := fmt.Sprintf("\"%s\"", self.Time.Format("2006-01-02 15:04:05-0700"))
+	s := fmt.Sprintf("\"%s\"", self.Time.Format(time.RFC3339))
 	return []byte(s), nil
 }
