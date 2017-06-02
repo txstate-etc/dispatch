2a2797cd779ffcfed3078bd940d5db119f4be30e
wickning1 <git@mythgaming.net>
2017-06-02 15:07:27 -0500

    added one more ssl cipher

--- main.go
+++ main.go
@@ -97,6 +97,7 @@ func main() {
 	if _, err := os.Stat("/ssl/dispatch.cert.pem"); err == nil {
 		cfg := &tls.Config{
 			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
