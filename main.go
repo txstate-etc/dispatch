25f32d6659be844046f4775c3a4219103320968e
Jeff Snider <jeff@jsnider.net>
2020-03-30 12:36:40 -0500

    Set minimum TLS version

--- main.go
+++ main.go
@@ -97,6 +97,7 @@ func main() {
 	r.HandleFunc("/settings/{token}", SettingsSet).Methods("POST")
 	if _, err := os.Stat("/ssl/dispatch.cert.pem"); err == nil {
 		cfg := &tls.Config{
+			MinVersion: tls.VersionTLS12,
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
